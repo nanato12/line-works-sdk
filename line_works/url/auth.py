f5ee5055f9826cf621d160828da5dc12b8daff9e
nanato12 <admin@okj.info>
2024-11-06 01:09:01 +0900

    update: AuthURL

--- line_works/url/auth.py
+++ line_works/url/auth.py
@@ -4,4 +4,5 @@ from urllib.parse import urljoin
 class AuthURL:
     HOST = "https://auth.worksmobile.com"
 
+    LOGIN = urljoin(HOST, "/login/login")
     LOGIN_PROCESS_V2 = urljoin(HOST, "/login/loginProcessV2")
