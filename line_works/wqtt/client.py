0f5ab6679c38b3f991ba06005a86421dbec60d64
nanato12 <admin@okj.info>
2025-01-03 02:03:51 +0900

    fix: type annotations

--- line_works/wqtt/client.py
+++ line_works/wqtt/client.py
@@ -20,7 +20,7 @@ class WMQTTClient(BaseModel):
     def cookie_str(self) -> str:
         return "; ".join(f"{k}={v}" for k, v in self.cookies.items())
 
-    async def connect(self):
+    async def connect(self) -> None:
         self._ws = await websockets.connect(
             config.HOST,
             ssl=create_default_context(),
@@ -35,12 +35,12 @@ class WMQTTClient(BaseModel):
             tg.create_task(self._send_pingreq())
             tg.create_task(self.listen())
 
-    async def _send_pingreq(self):
+    async def _send_pingreq(self) -> None:
         while True:
             await asyncio.sleep(config.KEEPALIVE_INTERVAL_SEC)
             await self._ws.send(packets.PINGREQ_PACKET)
 
-    async def listen(self):
+    async def listen(self) -> None:
         while True:
             message = await self._ws.recv()
             if isinstance(message, bytes):
@@ -48,6 +48,6 @@ class WMQTTClient(BaseModel):
             else:
                 print(f"テキストメッセージを受信: {message}")
 
-    async def _handle_binary_message(self, message):
+    async def _handle_binary_message(self, message: bytes) -> None:
         # TODO: 実装
         pass
