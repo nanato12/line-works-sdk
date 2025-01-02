18ef5611149a3419a24ba7123f371842eb8220d1
nanato12 <admin@okj.info>
2025-01-03 01:58:39 +0900

    update: wqtt client

---
+++ line_works/wqtt/__init__.py
@@ -0,0 +1,1 @@
+from line_works.wqtt.client import WMQTTClient
---
+++ line_works/wqtt/client.py
@@ -0,0 +1,53 @@
+import asyncio
+from ssl import create_default_context
+
+import websockets
+from pydantic import BaseModel, PrivateAttr
+from requests.cookies import RequestsCookieJar
+from websockets.asyncio.client import ClientConnection
+
+from line_works.wqtt import config, packets
+
+
+class WMQTTClient(BaseModel):
+    cookies: RequestsCookieJar
+    _ws: ClientConnection = PrivateAttr(default=None)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @property
+    def cookie_str(self) -> str:
+        return "; ".join(f"{k}={v}" for k, v in self.cookies.items())
+
+    async def connect(self):
+        self._ws = await websockets.connect(
+            config.HOST,
+            ssl=create_default_context(),
+            additional_headers={"Cookie": self.cookie_str, **config.HEADERS},
+            subprotocols=["mqtt"],
+            ping_interval=None,
+        )
+
+        await self._ws.send(packets.CONNECTION_PACKET)
+
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self._send_pingreq())
+            tg.create_task(self.listen())
+
+    async def _send_pingreq(self):
+        while True:
+            await asyncio.sleep(config.KEEPALIVE_INTERVAL_SEC)
+            await self._ws.send(packets.PINGREQ_PACKET)
+
+    async def listen(self):
+        while True:
+            message = await self._ws.recv()
+            if isinstance(message, bytes):
+                await self._handle_binary_message(message)
+            else:
+                print(f"テキストメッセージを受信: {message}")
+
+    async def _handle_binary_message(self, message):
+        # TODO: 実装
+        pass
