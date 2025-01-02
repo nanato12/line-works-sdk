f3a4640373c3d7ed053491e01b2942008b91d2e7
nanato12 <admin@okj.info>
2025-01-03 02:13:16 +0900

    update: config

--- line_works/wqtt/config.py
+++ line_works/wqtt/config.py
@@ -1,7 +1,9 @@
+from line_works.config import UA
+
 HOST = "wss://jp1-web-noti.worksmobile.com/wmqtt"
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-    "Origin": "https://talk.worksmobile.com",
-    "Sec-WebSocket-Protocol": "mqtt",
+    "user-agent": UA,
+    "origin": "https://talk.worksmobile.com",
+    "sec-websocket-protocol": "mqtt",
 }
 KEEPALIVE_INTERVAL_SEC = 10
