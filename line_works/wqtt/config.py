db07c44a4726685f2a3134b401ec6af306ea429b
nanato12 <admin@okj.info>
2025-01-03 01:56:29 +0900

    add: config

---
+++ line_works/wqtt/config.py
@@ -0,0 +1,7 @@
+HOST = "wss://jp1-web-noti.worksmobile.com/wmqtt"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Origin": "https://talk.worksmobile.com",
+    "Sec-WebSocket-Protocol": "mqtt",
+}
+KEEPALIVE_INTERVAL_SEC = 10
