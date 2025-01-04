2ceeca3015457afe57a80044c8f04bba617f2d5c
nanato12 <admin@okj.info>
2025-01-04 12:39:14 +0900

    add: SentMessage

---
+++ line_works/models/sent_message.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class SentMessage(BaseModel):
+    message_id: int = Field(alias="messageId")
+    channel_no: int = Field(alias="channelNo")
+    writer_id: str = Field(alias="writerId")
+    user_no: int = Field(alias="userNo")
+    bot_no: int = Field(alias="botNo")
+    message_no: int = Field(alias="messageNo")
+    content: str
+    member_count: int = Field(alias="memberCount")
+    message_type_code: int = Field(alias="messageTypeCode")
+    message_status_type: str = Field(alias="messageStatusType")
+    message_status_type_code: int = Field(alias="messageStatusTypeCode")
+    extras: Optional[str] = None
+    tid: int
+    create_time: int = Field(alias="createTime")
+    update_time: int = Field(alias="updateTime")
