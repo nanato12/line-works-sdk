e0f9531d403b0626ed558490cc5298fc4c532882
nanato12 <admin@okj.info>
2025-01-25 00:18:29 +0900

    update: MessagePayload

--- line_works/mqtt/models/payload/message.py
+++ line_works/mqtt/models/payload/message.py
@@ -3,6 +3,9 @@ from typing import Optional
 
 from pydantic import Field
 
+from line_works.exceptions import LogicException
+from line_works.models.sticker import Sticker
+from line_works.mqtt.enums.notification_type import NotificationType
 from line_works.mqtt.models.payload.badge import BadgePayload
 
 
@@ -29,3 +32,12 @@ class MessagePayload(BadgePayload):
     @property
     def extras_dict(self) -> dict:
         return json.loads(self.extras) if self.extras else {}
+
+    @property
+    def sticker(self) -> Sticker:
+        if self.notification_type == NotificationType.NOTIFICATION_STICKER:
+            return Sticker(**self.extras_dict)
+        raise LogicException(
+            f"Invalid notification type: {self.notification_type}. "
+            f"Expected {NotificationType.NOTIFICATION_STICKER}."
+        )
